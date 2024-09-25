b06a06ed567db9a95323e4f67ea66b10058920ca
Gustavo Lisboa Molina <80642295+GustavoLM2021@users.noreply.github.com>
2024-09-25 20:47:25 +0000

    Pending changes exported from your codespace

--- qemu-up.sh
+++ qemu-up.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-qemu-system-i386 --device e1000,netdev=eth0,mac=aa:bb:cc:dd:ee:ff \
+qemu-system-i386 -smp 4 \
+	--device e1000,netdev=eth0,mac=aa:bb:cc:dd:ee:ff \
 	--netdev tap,id=eth0,script=custom-scripts/qemu-ifup \
 	--kernel output/images/bzImage \
 	--hda output/images/rootfs.ext2 --nographic \
